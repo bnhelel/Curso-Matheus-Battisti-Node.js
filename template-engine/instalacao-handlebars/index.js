838042c16af02905fbf5399a731aa18160713b6b
Bn22 <bernardontoultimate@gmail.com>
2024-03-18 21:16:42 -0300

    Continuidade ao curso, criando a handlebars

--- template-engine/instalacao-handlebars/index.js
+++ template-engine/instalacao-handlebars/index.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const expbhs = require("express-handlebars");
+const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
-app.engine('handlebars', expbhs());
-app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => {
-    res.render('home', {layout: false})
-});
-
-app.listen(3000, () => {
-    console.log("Servidor ligado!")
+app.get('/', function (req, res) {
+  res.render('home', { layout: false })
 })
+
+app.listen(3001, () => {
+    console.log("Servidor funcionando!")
+});
