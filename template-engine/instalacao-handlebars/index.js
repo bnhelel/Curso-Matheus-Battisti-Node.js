32741bd03d827e82f115cde9189409e6ad5bc8d1
Bn22 <bernardontoultimate@gmail.com>
2024-03-21 17:11:22 -0300

    ultimo estudo sobre handlebars :)

--- template-engine/instalacao-handlebars/index.js
+++ template-engine/instalacao-handlebars/index.js
@@ -6,8 +6,20 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+app.get('/dashboard.handlebars', (req, res) => {
+  res.render('dashboard')
+});
+
 app.get('/', function (req, res) {
-  res.render('home', { layout: false })
+
+  const user = {
+    name: "Matheus",
+    surname:"Battisti",
+    age: 30,
+  }
+  const auth = true;
+
+  res.render('home', {user: user, palavra}); //método do express que será passado do back ao front!
 })
 
 app.listen(3001, () => {
